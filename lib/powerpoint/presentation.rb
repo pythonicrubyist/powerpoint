fad5820c91adb8bcfb0393de1568478847217393
pythonicrubyist <pythonicrubyist@gmail.com>
2014-03-11 16:12:43 -0400

    code cleanup.

--- lib/powerpoint/presentation.rb
+++ lib/powerpoint/presentation.rb
@@ -11,38 +11,44 @@ module Powerpoint
 
     def initialize path
       @pptx_path = path
+      @slide_count = 0
       @extract_path = path.split('.pptx').first + Time.now.strftime("%Y-%m-%d-%H%M%S")
       FileUtils.copy_entry TEMPLATE_PATH, @extract_path
     end
 
     def add_intro title, subtitile=nil
+      @slide_count += 1
+
       intro_slide_path = "#{@extract_path}/ppt/slides/slide1.xml"
-      temp = File.read intro_slide_path
+      template_xml = File.read intro_slide_path
 
       xml_title = '<a:p><a:r><a:rPr lang="en-US" dirty="0" smtClean="0"/><a:t>' + title.to_s + '</a:t></a:r><a:endParaRPr lang="en-US" dirty="0"/></a:p>'
-      temp.gsub!('PRESENTATION_TITLE_PACEHOLDER', xml_title)
+      template_xml.gsub!('PRESENTATION_TITLE_PACEHOLDER', xml_title)
 
       xml_subtitle = '<a:p><a:r><a:rPr lang="en-US" dirty="0" smtClean="0"/><a:t>' + subtitile.to_s+ '</a:t></a:r><a:endParaRPr lang="en-US" dirty="0"/></a:p>'
-      temp.gsub!('PRESENTATION_SUBTITLE_PACEHOLDER', xml_subtitle)
+      template_xml.gsub!('PRESENTATION_SUBTITLE_PACEHOLDER', xml_subtitle)
 
-      File.open(intro_slide_path, 'w'){ |f| f << temp }
+      File.open(intro_slide_path, 'w'){ |f| f << template_xml }
     end
 
     def add_textual_slide title, content=[]
-      slide_path = "#{@extract_path}/ppt/slides/slide2.xml"
-      temp = File.read slide_path
+      @slide_count += 1
+
+      slide_template_path = "#{@extract_path}/ppt/slides/slide2.xml"
+      template_xml = File.read slide_template_path
 
       title_xml = '<a:p><a:r><a:rPr lang="en-US" dirty="0" smtClean="0"/><a:t>'+ title.to_s + '</a:t></a:r><a:endParaRPr lang="en-US" dirty="0"/></a:p>'
-      temp.gsub!('SLIDE_TITLE_PACEHOLDER', title_xml)
+      template_xml.gsub!('SLIDE_TITLE_PACEHOLDER', title_xml)
 
       content_xml = ''
       content.each do |i|
         content_xml += '<a:p><a:r><a:rPr lang="en-US" dirty="0" smtClean="0"/><a:t>' + i.to_s + '</a:t></a:r></a:p>'
       end
 
-      temp.gsub!('CONTENT_PACEHOLDER', content_xml)
+      template_xml.gsub!('CONTENT_PACEHOLDER', content_xml)
 
-      File.open(slide_path, 'w'){ |f| f << temp }
+      slide_path = "#{@extract_path}/ppt/slides/slide#{@slide_count}.xml"
+      File.open(slide_path, 'w'){ |f| f << template_xml }
     end
 
     def add_pictorial_slide title, image_path
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,6 +2,7 @@ require 'powerpoint'
 
 describe 'Powerpoint parsing a sample PPTX file' do
   before(:all) do
+    #Powerpoint.decompress_pptx'blank.pptx', 'blank'
     @deck = Powerpoint::Presentation.new 'sample.pptx'
     @deck.add_intro 'Bicycle Of the Mind', 'created by Steve Jobs'
     @deck.add_textual_slide 'Why Mac?', ['Its cool!', 'Its light.']
