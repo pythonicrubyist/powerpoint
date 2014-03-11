773c12490899f1bbe01fed421b465bb43a99e54d
pythonicrubyist <pythonicrubyist@gmail.com>
2014-03-11 17:39:02 -0400

    relationship processing added.

--- lib/powerpoint/presentation.rb
+++ lib/powerpoint/presentation.rb
@@ -9,17 +9,16 @@ module Powerpoint
 
     attr_reader :pptx_path, :extract_path
 
-    def initialize path
-      @pptx_path = path
+    def initialize
       @slide_count = 0
-      @extract_path = path.split('.pptx').first + Time.now.strftime("%Y-%m-%d-%H%M%S")
+      @extract_path =  "extract_#{Time.now.strftime("%Y-%m-%d-%H%M%S")}"
       FileUtils.copy_entry TEMPLATE_PATH, @extract_path
     end
 
     def add_intro title, subtitile=nil
       @slide_count += 1
 
-      intro_slide_path = "#{@extract_path}/ppt/slides/slide1.xml"
+      intro_slide_path = "#{TEMPLATE_PATH}/ppt/slides/slide1.xml"
       template_xml = File.read intro_slide_path
 
       xml_title = '<a:p><a:r><a:rPr lang="en-US" dirty="0" smtClean="0"/><a:t>' + title.to_s + '</a:t></a:r><a:endParaRPr lang="en-US" dirty="0"/></a:p>'
@@ -34,7 +33,7 @@ module Powerpoint
     def add_textual_slide title, content=[]
       @slide_count += 1
 
-      slide_template_path = "#{@extract_path}/ppt/slides/slide2.xml"
+      slide_template_path = "#{TEMPLATE_PATH}/ppt/slides/slide2.xml"
       template_xml = File.read slide_template_path
 
       title_xml = '<a:p><a:r><a:rPr lang="en-US" dirty="0" smtClean="0"/><a:t>'+ title.to_s + '</a:t></a:r><a:endParaRPr lang="en-US" dirty="0"/></a:p>'
@@ -49,14 +48,23 @@ module Powerpoint
 
       slide_path = "#{@extract_path}/ppt/slides/slide#{@slide_count}.xml"
       File.open(slide_path, 'w'){ |f| f << template_xml }
+
+      if @slide_count > 2
+        relationship_xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideLayout" Target="../slideLayouts/slideLayout2.xml"/></Relationships>'
+        relationship_path = "#{@extract_path}/ppt/slides/_rels/slide#{@slide_count}.xml.rels"
+        File.open(relationship_path, 'w'){ |f| f << relationship_xml }
+      end
     end
 
     def add_pictorial_slide title, image_path
 
     end
 
-    def save
+    def save path
+      @pptx_path = path
       Powerpoint.compress_pptx @extract_path, @pptx_path
+      path
     end
   end
 end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,11 +2,14 @@ require 'powerpoint'
 
 describe 'Powerpoint parsing a sample PPTX file' do
   before(:all) do
-    #Powerpoint.decompress_pptx'blank.pptx', 'blank'
-    @deck = Powerpoint::Presentation.new 'sample.pptx'
+    # Powerpoint.decompress_pptx 'test2.pptx', 'test2'
+    # Powerpoint.decompress_pptx 'test1.pptx', 'test1'
+    # Powerpoint.compress_pptx 'test2', 'zpz.pptx'
+    @deck = Powerpoint::Presentation.new 
     @deck.add_intro 'Bicycle Of the Mind', 'created by Steve Jobs'
-    @deck.add_textual_slide 'Why Mac?', ['Its cool!', 'Its light.']
-    @deck.save
+    @deck.add_textual_slide 'Why Mac?', ['Its cool!', 'Its light!']
+    @deck.add_textual_slide 'Why Iphone?', ['Its fast!', 'Its cheap!']
+    @deck.save 'sample.pptx'
   end
 
   it 'Create a PPTX file successfully.' do
