f2907a621224751d6a0a1f31982e12159a319024
wbutler007 <william.l.butler@us.pwc.com>
2014-10-10 16:15:25 -0400

    added test

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -11,6 +11,7 @@ describe 'Powerpoint parsing a sample PPTX file' do
     @deck.add_pictorial_slide 'GIF Logo', 'samples/images/sample_gif.gif', {x: 124200, y: 3356451, cx: 2895600, cy: 1013460}
     @deck.add_pictorial_slide 'SVG Logo', 'samples/images/sample_svg.svg'
     @deck.add_textual_slide 'Why Android?', ['Its great!', 'Its sweet!']
+    @deck.add_table_slide "Table test", [["foo","bar","baz"],["1","2","3"]]
     @deck.save 'samples/pptx/sample.pptx' # Examine the PPTX file
   end
 
