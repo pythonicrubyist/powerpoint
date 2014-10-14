e46bbbe235214de7c0d1135b0eeb5a8db9518f8e
wbutler007 <william.l.butler@us.pwc.com>
2014-10-14 14:17:06 -0500

    added utils::table to abstract table xml gen

--- lib/powerpoint/slide/table.rb
+++ lib/powerpoint/slide/table.rb
@@ -5,53 +5,13 @@ module Powerpoint
   module Slide
     class Powerpoint::Slide::Table
       def initialize extract_path, title, table, slide_number
-        #table has to have rows
-        raise ArgumentError, "Table must have rows" if table.length == 0
-
-        #get number of cols for the gridcol tags
-        ncols = table.first.length
-
         template_path = "#{TEMPLATE_PATH}/ppt/slides/slide3.xml"
         xml = File.read template_path
 
         #set the title
         xml.gsub!("TITLE_PLACEHOLDER", title)
 
-        #set the gridcol tags
-        cols_xml = ""
-        ncols.times do |n|
-          cols_xml << "<a:gridCol w='1645920' />"
-        end
-        xml.gsub! "COLS_PLACEHOLDER", cols_xml
-
-        #make the actual rows
-        table_xml = table.map do |row|
-          %{
-			<a:tr h="370840">
-				#{
-					row.map do |col|
-						%{
-							<a:tc>
-								<a:txBody>
-									<a:bodyPr />
-									<a:lstStyle />
-									<a:p>
-										<a:r>
-											<a:rPr lang="en-US" dirty="0" smtClean="0" />
-											<a:t>#{col}</a:t>
-										</a:r>
-										<a:endParaRPr lang="en-US" dirty="0" />
-									</a:p>
-								</a:txBody>
-								<a:tcPr />
-							</a:tc>
-						}
-					end.join
-				}
-			</a:tr>
-		}
-        end.join
-        xml.gsub! "ROWS_PLACEHOLDER", table_xml
+        xml.gsub! "TABLE_PLACEHOLDER", table.to_xml
 
         #write the result to the powerpoint
         slide_path = "#{extract_path}/ppt/slides/slide#{slide_number}.xml"
---
+++ lib/powerpoint/utils/table.rb
@@ -0,0 +1,78 @@
+
+module Powerpoint
+  module Utils
+    class Table
+      def initialize matrix, settings = {}
+        raise ArgumentError.new("no rows") if matrix.first.nil?
+        raise ArgumentError.new("no cols") if matrix.first.first.nil?
+        
+        @matrix   = matrix
+        @ncols    = matrix
+        @width    = (settings[:width]    or "1645920")
+        @style    = (settings[:style]    or "5C22544A-7EE6-4342-B048-85BDC9FD1C3A")
+        @firstrow = (settings[:firstrow] or "1")
+        @bandrow  = (settings[:bandrow]  or "1")
+        
+      end
+
+      def width=(width)
+        @width = width
+      end
+
+      def style=(style)
+        @style = style
+      end
+
+      def firstrow=(firstrow)
+        @firstrow = firstrow
+      end
+
+      def bandrow=(bandrow)
+        @bandrow = bandrow
+      end
+
+      def to_xml
+		%{
+			<a:tbl>
+				<a:tblPr firstRow="#{@firstrow}" bandRow="">
+					<a:tableStyleId>#{@heading}</a:tableStyleId>
+				</a:tblPr>
+				<a:tblGrid>
+					#{
+						"<a:gridCol w='#{@width}' />"*@ncols
+					}
+				</a:tblGrid>
+				#{
+					table.map do |row|
+						%{
+							<a:tr h="370840">
+								#{
+									row.map do |col|
+										%{
+											<a:tc>
+												<a:txBody>
+													<a:bodyPr />
+													<a:lstStyle />
+													<a:p>
+														<a:r>
+															<a:rPr lang="en-US" dirty="0" smtClean="0" />
+															<a:t>#{col}</a:t>
+														</a:r>
+														<a:endParaRPr lang="en-US" dirty="0" />
+													</a:p>
+												</a:txBody>
+												<a:tcPr />
+											</a:tc>
+										}
+									end.join
+								}
+							</a:tr>
+						}
+					end.join
+				}
+			</a:tbl>
+		}
+      end
+    end
+  end
+end
