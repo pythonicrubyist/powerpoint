e525850e44bb859212995c0e66fcd3ce9d2f5636
Bob Foreman <bob@sebazzo.com>
2021-01-11 13:55:19 +0100

    remove unneccesary slides

--- lib/powerpoint/slide/image.rb
+++ lib/powerpoint/slide/image.rb
@@ -41,12 +41,12 @@ module Powerpoint
       private :default_coords
 
       def save_rel_xml(extract_path, index)
-        render_view('ranking_rel.xml.erb', "#{extract_path}/ppt/slides/_rels/slide#{index}.xml.rels", index: index)
+        render_view('image_rel.xml.erb', "#{extract_path}/ppt/slides/_rels/slide#{index}.xml.rels", index: index)
       end
       private :save_rel_xml
 
       def save_slide_xml(extract_path, index)
-        render_view('ranking.xml.erb', "#{extract_path}/ppt/slides/slide#{index}.xml")
+        render_view('image.xml.erb', "#{extract_path}/ppt/slides/slide#{index}.xml")
       end
       private :save_slide_xml
     end
--- lib/powerpoint/slide/multiple_image.rb
+++ lib/powerpoint/slide/multiple_image.rb
@@ -28,12 +28,12 @@ module Powerpoint
       end
 
       def save_rel_xml(extract_path, index)
-        render_view('ranking_rel.xml.erb', "#{extract_path}/ppt/slides/_rels/slide#{index}.xml.rels", index: index)
+        render_view('multiple_image_rel.xml.erb', "#{extract_path}/ppt/slides/_rels/slide#{index}.xml.rels", index: index)
       end
       private :save_rel_xml
 
       def save_slide_xml(extract_path, index)
-        render_view('ranking.xml.erb', "#{extract_path}/ppt/slides/slide#{index}.xml")
+        render_view('multiple_image.xml.erb', "#{extract_path}/ppt/slides/slide#{index}.xml")
       end
       private :save_slide_xml
     end
