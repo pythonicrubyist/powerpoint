9a4ceb6a2c1e476fedcfd107910cd4eb2ddc343a
Bob Foreman <bob@sebazzo.com>
2021-01-12 17:06:42 +0100

    1st template

--- lib/powerpoint/slide/extended_intro.rb
+++ lib/powerpoint/slide/extended_intro.rb
@@ -8,17 +8,19 @@ module Powerpoint
     class ExtendedIntro
       include Powerpoint::Util
 
-      attr_reader :title, :subtitle, :subtitle_2, :image_name, :coords, :image_path
+      attr_reader :title, :subtitle, :subtitle_2, :image_name, :coords, :image_path, :image_path_2
 
       def initialize(options={})
-        require_arguments [:title, :subtitle, :image_path, :subtitle_2], options
+        require_arguments [:title, :subtitle, :image_path,:image_path_2, :subtitle_2], options
         options.each {|k, v| instance_variable_set("@#{k}", v)}
         @coords = default_coords unless @coords.any?
         @image_name = File.basename(@image_path)
+        @image_name_2 = File.basename(@image_path_2)
       end
 
       def save(extract_path, index)
         copy_media(extract_path, @image_path)
+        copy_media(extract_path, @image_path_2)
         save_rel_xml(extract_path, index)
         save_slide_xml(extract_path, index)
       end
