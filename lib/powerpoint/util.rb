37268c32be20b5f8c7fbec110d406c1b84f65dcc
Bob Foreman <bob@sebazzo.com>
2021-01-19 16:04:44 +0100

    1st template

--- lib/powerpoint/util.rb
+++ lib/powerpoint/util.rb
@@ -11,7 +11,7 @@ module Powerpoint
       b = merge_variables(binding, variables)
       data = renderer.result(b)
 
-      File.open(path, 'w') { |f| f << data }
+      File.open(path, 'w') { |f| f << data.squish! }
     end
 
     def read_template(filename)
