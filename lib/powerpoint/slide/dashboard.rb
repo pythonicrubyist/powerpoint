ba9c74113e29cd76453179915b1af791a9dc5350
Bob Foreman <bob@sebazzo.com>
2021-01-27 17:43:35 +0100

    gender symbols

--- lib/powerpoint/slide/dashboard.rb
+++ lib/powerpoint/slide/dashboard.rb
@@ -8,19 +8,23 @@ module Powerpoint
     class Dashboard
       include Powerpoint::Util
 
-      attr_reader :title, :subtitle, :subtitle_2, :image_path, :image_path_2, :image_name, :data
+      attr_reader :title, :subtitle, :subtitle_2, :image_path, :image_path_2,:image_path_3,:image_path_4, :image_name, :data
 
       def initialize(options={})
-        require_arguments [:title, :subtitle, :image_path, :image_path_2, :subtitle_2, :data], options
+        require_arguments [:title, :subtitle, :image_path, :image_path_2,:image_path_3, :image_path_4, :subtitle_2, :data], options
         options.each {|k, v| instance_variable_set("@#{k}", v)}
         @data = data
         @image_name = File.basename(@image_path)
         @image_name_2 = File.basename(@image_path_2)
+        @image_name_3 = File.basename(@image_path_3)
+        @image_name_4 = File.basename(@image_path_4)
       end
 
       def save(extract_path, index)
         copy_media(extract_path, @image_path)
         copy_media(extract_path, @image_path_2)
+        copy_media(extract_path, @image_path_3)
+        copy_media(extract_path, @image_path_4)
         save_rel_xml(extract_path, index)
         save_slide_xml(extract_path, index)
       end
