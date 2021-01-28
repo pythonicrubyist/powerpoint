90637a953e681fb38e269804dd3f1d95b4428825
Bob Foreman <bob@sebazzo.com>
2021-01-28 15:19:28 +0100

    dashboard 2

--- lib/powerpoint/presentation.rb
+++ lib/powerpoint/presentation.rb
@@ -61,7 +61,7 @@ module Powerpoint
     end
 
     def add_dashboard_users_slide(title, subtitle = nil, subtitle_2 = nil, images)
-      @slides << Powerpoint::Slide::DashboardUsers.new(presentation: self, title: title, subtitle: subtitle, subtitle_2: subtitle_2, images: data)
+      @slides << Powerpoint::Slide::DashboardUsers.new(presentation: self, title: title, subtitle: subtitle, subtitle_2: subtitle_2, images: images)
     end
 
     def save(path)
