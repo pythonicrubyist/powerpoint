cabdf0426269a44d38c32f53ad37d17d9b9c9e1c
Bob Foreman <bob@sebazzo.com>
2021-02-01 12:13:51 +0100

    subtitles for graphs

--- lib/powerpoint/slide/dashboard.rb
+++ lib/powerpoint/slide/dashboard.rb
@@ -8,10 +8,10 @@ module Powerpoint
     class Dashboard
       include Powerpoint::Util
 
-      attr_reader :title, :subtitle, :subtitle_2, :graph_1_title, :graph_1_subtitle :graph_2_title, :graph_2_subtitle, :image_path, :image_path_2,:image_path_3,:image_path_4, :image_name, :data
+      attr_reader :title, :subtitle, :subtitle_2, :graph_1_title, :graph_1_subtitle, :graph_2_title, :graph_2_subtitle, :image_path, :image_path_2,:image_path_3,:image_path_4, :image_name, :data
 
       def initialize(options={})
-        require_arguments [:title, :subtitle, :subtitle_2, ,:graph_1_title, :graph_1_subtitle :graph_2_title, :graph_2_subtitle, :image_path, :image_path_2,:image_path_3, :image_path_4, :data], options
+        require_arguments [:title, :subtitle, :subtitle_2, ,:graph_1_title, :graph_1_subtitle, :graph_2_title, :graph_2_subtitle, :image_path, :image_path_2,:image_path_3, :image_path_4, :data], options
         options.each {|k, v| instance_variable_set("@#{k}", v)}
         @data = data
         @image_name = File.basename(@image_path)
