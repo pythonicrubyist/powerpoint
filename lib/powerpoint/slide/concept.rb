7a1049d856680a3be9684e8845359402ffd0c819
Bob Foreman <bob@sebazzo.com>
2021-02-08 14:14:07 +0100

    concept eval

--- lib/powerpoint/slide/concept.rb
+++ lib/powerpoint/slide/concept.rb
@@ -8,13 +8,13 @@ module Powerpoint
     class Concept
       include Powerpoint::Util
 
-      attr_reader :title, :subtitle,:subtitle_2, :logo, :image_ratio :images
+      attr_reader :title, :subtitle,:subtitle_2, :logo, :image_information, :images
 
       def initialize(options={})
-        require_arguments [:title, :subtitle,:subtitle_2, :logo, :image_ratio, :images], options
+        require_arguments [:title, :subtitle,:subtitle_2, :logo, :image_information, :images], options
         options.each {|k, v| instance_variable_set("@#{k}", v)}
         @images = images
-        @image_ratio
+         
       end
 
       def save(extract_path, index)
@@ -26,6 +26,14 @@ module Powerpoint
 
         save_rel_xml(extract_path, index)
         save_slide_xml(extract_path, index)
+      end
+
+      def image_ratio(image_information)
+        if image_information[0] > image_information[1]
+          image_ratio = ['width',image_information[2]]
+        else
+          image_ratio = ['height',image_information[2]]
+        end
       end
 
       def save_rel_xml(extract_path, index)
