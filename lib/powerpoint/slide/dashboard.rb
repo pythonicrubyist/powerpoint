0fb721187298894c0b769f6c41a8d81dd6e21cdb
Bob Foreman <bob@sebazzo.com>
2021-02-15 10:50:23 +0100

    style multi images

--- lib/powerpoint/slide/dashboard.rb
+++ lib/powerpoint/slide/dashboard.rb
@@ -14,19 +14,19 @@ module Powerpoint
         require_arguments [:title, :subtitle, :graph_1_title, :graph_1_subtitle, :graph_2_title, :graph_2_subtitle, :image_path, :image_path_2,:image_path_3, :image_path_4, :logo, :data], options
         options.each {|k, v| instance_variable_set("@#{k}", v)}
         @data = data
-        @image_name = File.basename(@image_path)
-        @image_name_2 = File.basename(@image_path_2)
-        @image_name_3 = File.basename(@image_path_3)
-        @image_name_4 = File.basename(@image_path_4)
-        @logo = File.basename(@logo)
+        @image_name = File.basename(image_path)
+        @image_name_2 = File.basename(image_path_2)
+        @image_name_3 = File.basename(image_path_3)
+        @image_name_4 = File.basename(image_path_4)
+        @logo = File.basename(logo)
       end
 
       def save(extract_path, index)
-        copy_media(extract_path, @image_path)
-        copy_media(extract_path, @image_path_2)
-        copy_media(extract_path, @image_path_3)
-        copy_media(extract_path, @image_path_4)
-        copy_media(extract_path, @logo)
+        copy_media(extract_path, image_path)
+        copy_media(extract_path, image_path_2)
+        copy_media(extract_path, image_path_3)
+        copy_media(extract_path, image_path_4)
+        copy_media(extract_path, logo)
         save_rel_xml(extract_path, index)
         save_slide_xml(extract_path, index)
       end
